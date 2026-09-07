b6539328bcf3672802b610fd39f6f68cf4ff26f6
nsprempk <prempathak903@gmail.com>
2026-09-08 01:49:14 +0530

    Initial Learn Typing project

--- backend/server.js
+++ backend/server.js
@@ -71,7 +71,7 @@ app.use(notFound);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
